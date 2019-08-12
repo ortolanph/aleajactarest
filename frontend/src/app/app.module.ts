56a72a7f17c3e2537fdaaf203eccdf20ee4113ac
Paulo Henrique Ortolan <paulo.ortolan@gmail.com>
2019-08-12 16:47:46 +0200

    Building frontedn

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SimpleComponent } from './simple/simple.component';
+import { CupComponent } from './cup/cup.component';
+import { CustomComponent } from './custom/custom.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleComponent,
+    CupComponent,
+    CustomComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/cup/cup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cup',
+  templateUrl: './cup.component.html',
+  styleUrls: ['./cup.component.css']
+})
+export class CupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
